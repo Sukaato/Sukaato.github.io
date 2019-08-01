d9f046b725ed4a9de43ed2ae79ac1a3b774790d8
Sukaato <sukaato.dev@gmail.com>
2019-08-01 23:37:24 +0200

    Make home page

---
+++ src/js/webList.js
@@ -0,0 +1,26 @@
+const WEB_LIST = [
+    {
+        name: "League Of Sport",
+        url: "league-of-sport",
+        image: "png",
+        slogan: "Faire du sport en jouant à League of Legends c'est possible !"
+    }, {
+        empty: true
+    }, {
+        empty: true
+    }, {
+        empty: true
+    }, {
+        empty: true
+    }, {
+        empty: true
+    }, {
+        empty: true
+    }, {
+        empty: true
+    }, {
+        empty: true
+    }, {
+        empty: true
+    }
+]
