f648c4f92c5f401403225ee7c1ef17077cde24d4
Sukaato <sukaato.dev@gmail.com>
2019-08-22 21:45:14 +0200

    Little change for adaptation to the correct path for github page

--- src/js/webList.js
+++ src/js/webList.js
@@ -1,7 +1,7 @@
 const WEB_LIST = [
     {
         name: "League Of Sport",
-        url: "league-of-sport",
+        url: "League-Of-Sport",
         image: "png",
         slogan: "Faire du sport en jouant à League of Legends c'est possible !"
     }
